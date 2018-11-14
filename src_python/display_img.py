414a0b32c6090acab5c58ce3287bd6ff334895d4
larycoder <lenhuchuhiep99@gmail.com>
2018-11-14 13:35:58 +0700

    face_detected_video_v1

--- src_python/display_img.py
+++ src_python/display_img.py
@@ -7,6 +7,13 @@ face=haar_engine.detectMultiScale(grayImg,1.1,5)
 print('face found: ',len(face))
 for (x,y,w,h) in face:
     cv2.rectangle(test1,(x,y),(x+w,y+h),(0,255,0),2)
-cv2.imshow('show img python',test1)
-cv2.waitKey(0)
+cv2.resizeWindow('image',300,600)
+cv2.imshow('image',test1)
+key=cv2.waitKey(0)
+while(True):
+    key=cv2.waitKey(0)
+    if key==ord('s'):
+        break
+    else:
+        continue
 cv2.destroyAllWindows()

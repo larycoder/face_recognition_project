930e55711ead72030d7a5ef461154ce15ee2e663
lazycoder <32628992+larycoder@users.noreply.github.com>
2018-11-20 09:20:05 +0700

    face_recognition_fini_add_image-show_v1

--- face_recognition_windows/src/face_Predict.py
+++ face_recognition_windows/src/face_Predict.py
@@ -2,9 +2,19 @@ from face_train_LBPH import LBPH_face_trained
 from face_detected_haar import*
 import cv2
 
+def face_draw_text(img,matrix,text):
+  for (x,y,w,h) in matrix:
+    cv2.putText(img,text,(x,y),cv2.FONT_HERSHEY_PLAIN,1.5, (0, 255, 0), 2)
+
 path='../../image/video.mp4'
 video=cv2.VideoCapture(path)
 ret,frame=video.read()
 gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-label_predict=LBPH_face_trained().predict(gray)
-print(label_predict)
+face_matrix= ImageDetect(gray,1)
+(x,y,w,h)=face_matrix[0]
+face_compare=gray[y:y+w,x:x+h]
+label_predict=LBPH_face_trained().predict(face_compare)
+ImageDetect(frame)
+face_draw_text(frame,face_matrix,label_predict)
+cv2.imshow('image',frame)
+
